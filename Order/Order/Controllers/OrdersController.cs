446fbdaa8839c0244242d31d8cd752288f324139
angus676880 <angus676880@gmail.com>
2017-04-07 01:47:55 +0800

    read

---
+++ Order/Order/Controllers/OrdersController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Order.Models;
+using LinqKit;
+
+namespace Order.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private TSQL2012Entities db = new TSQL2012Entities();
+
+        // GET: Orders
+        public ActionResult Index(string companyName)
+        {
+            IQueryable<Orders> orders = db.Orders;
+
+            var pred = PredicateBuilder.True<Orders>();
+
+            if (!String.IsNullOrEmpty(companyName))
+            {
+                pred = pred.And(x => x.Customers.CompanyName.Contains(companyName));
+            }
+            return View(orders.AsExpandable().Where(pred).Include(o => o.Employees).Include(o => o.Customers).Include(o => o.Shippers).ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Orders orders = db.Orders.Find(id);
+            if (orders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orders);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "LastName");
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CompanyName");
+            ViewBag.ShipperID = new SelectList(db.Shippers, "ShipperID", "CompanyName");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrderID,CustomerID,EmployeeID,OrderDate,RequiredDate,ShippedDate,ShipperID,Freight,ShipName,ShipAddress,ShipCity,ShipRegion,ShipPostalCode,ShipCountry")] Orders orders)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(orders);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "LastName", orders.EmployeeID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CompanyName", orders.CustomerID);
+            ViewBag.ShipperID = new SelectList(db.Shippers, "ShipperID", "CompanyName", orders.ShipperID);
+            return View(orders);
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Orders orders = db.Orders.Find(id);
+            if (orders == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "LastName", orders.EmployeeID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CompanyName", orders.CustomerID);
+            ViewBag.ShipperID = new SelectList(db.Shippers, "ShipperID", "CompanyName", orders.ShipperID);
+            return View(orders);
+        }
+
+        // POST: Orders/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrderID,CustomerID,EmployeeID,OrderDate,RequiredDate,ShippedDate,ShipperID,Freight,ShipName,ShipAddress,ShipCity,ShipRegion,ShipPostalCode,ShipCountry")] Orders orders)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orders).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "LastName", orders.EmployeeID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CompanyName", orders.CustomerID);
+            ViewBag.ShipperID = new SelectList(db.Shippers, "ShipperID", "CompanyName", orders.ShipperID);
+            return View(orders);
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Orders orders = db.Orders.Find(id);
+            if (orders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orders);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Orders orders = db.Orders.Find(id);
+            db.Orders.Remove(orders);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
